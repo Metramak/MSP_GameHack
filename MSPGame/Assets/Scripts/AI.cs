c31e370e79c12e8b73b1152cf98d98211c24a907
KesuTain <brodin.sokol@yandex.ru>
2019-04-20 17:27:01 +0300

    Управление + анимация

---
+++ MSPGame/Assets/Scripts/AI.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI : MonoBehaviour
+{
+
+    private Vector3 newPointForPlayer;
+    public NavMeshAgent agent;
+    private GameObject clone;
+    public GameObject spawnObject;
+    public bool patrule;
+    private bool checkSpawnPoints;
+
+    void Start()
+    {
+        patrule = true;
+        checkSpawnPoints = true;
+    }
+
+    void Update()
+    {
+        if (patrule == true)
+        {
+            if (checkSpawnPoints == true)
+            {
+                checkSpawnPoints = false;
+                spawnPoints();
+            }
+            if (Vector3.Distance(transform.position, clone.transform.position) <= 1.5f)
+            {
+                Destroy(clone);
+                checkSpawnPoints = true;
+            }
+        }
+        agent.SetDestination(newPointForPlayer);
+    }
+    void spawnPoints()
+    {
+        Vector3 truePos = transform.position;
+        NavMeshHit hit;
+        do
+        {
+            float xcor = Random.Range(-1000f, 1000f);
+            float ycor = 0.125f;
+            float zcor = Random.Range(-1000f, 1000f);
+            truePos = new Vector3(xcor, ycor, zcor);
+        }
+        while (NavMesh.SamplePosition(truePos, out hit, 1f, NavMesh.AllAreas) == false);
+        clone = Instantiate(spawnObject, truePos, Quaternion.identity);
+        newPointForPlayer = clone.transform.position;
+    }
+}
---
+++ MSPGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public LayerMask Layer_Mask;
+    private Vector3 newPoint;
+    public GameObject player;
+    public float speed = 7f;
+    public Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 1000, Layer_Mask))
+        {
+            newPoint = hit.point;
+            newPoint.y += 1.5f;
+        }
+        player.transform.LookAt(newPoint);
+        if (Input.GetKey(KeyCode.W))
+        {
+            player.transform.position += Vector3.forward * Time.deltaTime * speed;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed += 15f;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed -= 15f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            player.transform.position += -Vector3.forward * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            player.transform.position += Vector3.right * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            player.transform.position += Vector3.left * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.W) | Input.GetKey(KeyCode.S) | Input.GetKey(KeyCode.D) | Input.GetKey(KeyCode.A))
+        {
+            animator.SetBool("Run", true);
+        }
+        else if (Input.GetKeyUp(KeyCode.W) | Input.GetKeyUp(KeyCode.S) | Input.GetKeyUp(KeyCode.D) | Input.GetKeyUp(KeyCode.A))
+        {
+            animator.SetBool("Run", false);
+        }
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            
+        }
+    }
+}
