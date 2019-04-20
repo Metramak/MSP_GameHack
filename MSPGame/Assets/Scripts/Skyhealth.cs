3e65572a3c19316d7285128dbba2d761d62c1d72
Metramak <44141304+Metramak@users.noreply.github.com>
2019-04-20 17:25:14 +0300

    Added UI

---
+++ MSPGame/Assets/Scripts/Skyhealth.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Skyhealth : MonoBehaviour
+{
+    public float healthlength = 232;
+    public GameObject healthstring;
+    public GameObject healthbar;
+    public bool recoverhealth;
+    public float weakSpell = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SimulateHealth());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthstring.GetComponent<RectTransform>().sizeDelta = new Vector2(healthlength, 20);
+
+        if(recoverhealth)
+        {
+            if(healthlength >= 232)
+            {
+                recoverhealth = false;
+                healthbar.SetActive(false);
+
+            }
+            else
+            {
+                healthlength += weakSpell;
+            }
+        }
+    }
+
+    IEnumerator SimulateHealth()
+    {
+        yield return new WaitForSeconds(5);
+        healthlength -= 30;
+        yield return new WaitForSeconds(2);
+        healthlength -= 20;
+        yield return new WaitForSeconds(3);
+        healthlength -= 40;
+        yield return new WaitForSeconds(3);
+        recoverhealth = true;
+        yield return new WaitForSeconds(10);    
+        healthbar.SetActive(true);
+        healthlength -= 232;
+    }
+}
