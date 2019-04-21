6c66b6d47d9175424110bbb32d2ee8ad985af1d2
Metramak <44141304+Metramak@users.noreply.github.com>
2019-04-21 15:06:55 +0300

    Added PauseMenu, Cut Scene Skyrim

---
+++ MSPGame/Assets/Scripts/Pause.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    public float timer;
+    public bool ispause;
+    public bool guipause;
+    
+    void Update()
+    {
+        Time.timeScale = timer;
+        
+        if(Input.GetKeyDown(KeyCode.Escape) && !ispause)
+        {
+            ispause = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && ispause)
+        {
+            ispause = false;
+        }
+
+        if (ispause)
+        {
+            timer = 0;
+            guipause = true;
+        }
+        else if (!ispause)
+        {
+            timer = 1f;
+            guipause = false;
+        }
+    }
+
+    public void OnGUI()
+    {
+        if (guipause)
+        {
+            Cursor.visible = true;
+            if (GUI.Button(new Rect((float)(Screen.width / 2), (float)(Screen.height / 2) - 150f, 150f, 45f), "Continue"))
+            {
+                ispause = false;
+                timer = 0;
+                Cursor.visible = false;
+            }
+            if(GUI.Button(new Rect((float)(Screen.width / 2), (float)(Screen.height / 2) -50f, 150f, 45f), "Main Menu"))
+            {
+                ispause = false;
+                timer = 0;
+                SceneManager.LoadScene("Main Menu", LoadSceneMode.Single);
+            }
+        }
+    }
+}
